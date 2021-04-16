bd6e302cbe6dc960e7f5ea44b5e9d6b005513386
邓新 <dengxin@dakale.net>
2021-04-16 11:32:40 +0800

    代码 修改 debug

--- src/common/utils.js
+++ src/common/utils.js
@@ -18,7 +18,7 @@ import { View } from "@tarojs/components";
 import Router from "./router";
 // import moment from 'moment'
 export const navigateTo = (url, events) => {
-  console.log(url)
+  console.log(url);
   Taro.navigateTo({
     url: url,
     events: events || {},
@@ -576,16 +576,14 @@ export const computedClient = () => {
 //tags 排版
 
 export const loginStatus = () => {
-  if (
-    Taro.getStorageSync("userInfo") &&
-    Taro.getStorageSync("userInfo").mobile.length === 11 &&
-    Taro.getStorageSync("userInfo").token
-  ) {
+  const { token = "", mobile = "" } = Taro.getStorageSync("userInfo") || {};
+  if (mobile.length === 11 && token) {
     return Taro.getStorageSync("userInfo");
   } else {
     return false;
   }
 };
+
 export const format = (time = "") => {
   if (new Date().getTime() > new Date(time.replace(/-/g, "/")).getTime()) {
     return true;
@@ -606,4 +604,3 @@ export const computedPrice = (price, scale) => {
   let size = (price * (scale / 100)).toFixed(3);
   return size.substring(0, size.length - 1);
 };
-
