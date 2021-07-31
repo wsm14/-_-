66edb7c16130e45e156adbfac54685f5bd8e4aa7
邓新 <dengxin@dakale.net>
2021-07-31 16:28:20 +0800

    no message

--- src/api/newRequest.js
+++ src/api/newRequest.js
@@ -55,8 +55,8 @@ switch (env) {
     // baseUrl = "https://gateway1.dakale.net";
     break;
   case "production":
-    // baseUrl = "https://pregateway.dakale.net";
-    baseUrl = "https://devgateway.dakale.net";
+    baseUrl = "https://pregateway.dakale.net";
+    // baseUrl = "https://devgateway.dakale.net";
     // baseUrl = "https://gateway1.dakale.net";
     break;
 }
