2e4b062f9d5746649252b336d2e0058bfd239af5
邓新 <dengxin@dakale.net>
2021-08-31 17:46:08 +0800

    商品编辑

--- src/pages/relay/personal/GoodsManage/Edit/index.jsx
+++ src/pages/relay/personal/GoodsManage/Edit/index.jsx
@@ -4,14 +4,20 @@ import { useRouter } from "@tarojs/taro";
 import evens from "@/common/evens";
 import { getAuthStatus } from "@/common/authority";
 import { View, Button } from "@tarojs/components";
-import { Form, Input, Text, Upload } from "@/relay/components/FormCondition";
+import {
+  Form,
+  Input,
+  Text,
+  Upload,
+  Textarea,
+} from "@/relay/components/FormCondition";
 import { mapSelect, toast } from "@/common/utils";
 import { upload } from "@/api/upload";
 import FooterFixed from "@/relay/components/FooterFixed";
 import {
-  fetchLiftingCabinetEdit,
-  fetchLiftingCabinetCreate,
-  fetchLiftingCabinetDetail,
+  fetchCommonGoods,
+  fetchUpdateCommunityGoods,
+  fetchCommonGoodsDetail,
 } from "@/server/relay";
 import "./index.scss";
 
@@ -24,7 +30,7 @@ const FormItemGroup = Form.Group;
 export default () => {
   // 路由获取参数
   const routeParams = useRouter().params;
-  const { type, ownerId, communityLiftingCabinetId } = routeParams;
+  const { type, ownerId, communityGoodsId } = routeParams;
 
   const [formData, setFormData] = useState({});
 
@@ -36,11 +42,9 @@ export default () => {
 
   // 获取详情
   const fetchGetDetail = () => {
-    fetchLiftingCabinetDetail({ ownerId, communityLiftingCabinetId }).then(
-      (res) => {
-        setFormData(res.communityLiftingCabinetInfo);
-      }
-    );
+    fetchCommonGoodsDetail({ ownerId, communityGoodsId }).then((res) => {
+      setFormData(res.communityCommonGoods);
+    });
   };
 
   // 信息储存
@@ -48,32 +52,26 @@ export default () => {
 
   // 提交保存
   const handleSaveData = (value) => {
-    const { liftingName } = value;
-    const { lnt, lat, address, images = "" } = formData;
-    if (!liftingName) {
-      toast("请输入自提点名称");
+    const { goodsName, costPrice, desc } = value;
+    const { img = [] } = formData;
+    if (!goodsName) {
+      toast("请填写商品名称");
       return;
-    } else if (!lnt || !lat) {
-      toast("请选择位置");
-      return;
-    } else if (!address) {
-      toast("请选择地址");
+    } else if (!costPrice) {
+      toast("请填写售价");
       return;
     }
-    const img = images
-      ? Array.isArray(images)
-        ? images
-        : images.split(",")
-      : [];
-    upload(img, { img: img }).then((res) => {
+
+    const imgs = img ? (Array.isArray(img) ? img : img.split(",")) : [];
+    upload(imgs, { img: imgs }).then((res) => {
       const fetch = {
-        add: fetchLiftingCabinetCreate, // 新增
-        edit: fetchLiftingCabinetEdit, // 修改
+        add: fetchCommonGoods, // 新增
+        edit: fetchUpdateCommunityGoods, // 修改
       }[type];
       fetch({
         ...formData,
         ...value,
-        images: res.img.toString(),
+        communityGoodsDescObject: { img: img.toString(), desc },
       }).then(() => {
         toast("编辑成功");
         Taro.navigateBack({ delta: 1 });
@@ -82,82 +80,86 @@ export default () => {
   };
 
   return (
-    <View className="SelfLiftingPointEdit_Form">
+    <View className="GoodsManage_box">
       <Form
         borderRadius={false}
         onSubmit={(e) => handleSaveData(e.detail.value)}
         footer={false}
       >
-        <FormItemGroup>
-          <FormItem label={"自提点名称"} required>
-            <Input
-              value={formData.liftingName}
-              name="liftingName"
-              placeholder="自提点简称"
-            ></Input>
-          </FormItem>
-          <FormItem label={"位置"} required>
-            <Text
-              value={formData.location}
-              placeholder="自提点位置"
-              onClick={() => {
-                getAuthStatus({
-                  key: "location",
-                  success: () => {
-                    mapSelect((val) => {
-                      const { address, ...other } = val;
-                      savaFormData(other);
-                    });
-                  },
-                  fail: () => {
-                    evens.$emit("setLocation");
-                  },
-                });
-              }}
-              rightIcon={false}
-              extra={<View className="slp_edit_local"></View>}
-            ></Text>
-          </FormItem>
-          <FormItem label={"详细地址"} required>
-            <Text
-              value={formData.address}
-              placeholder="详细地址"
-              rightIcon={false}
-              onClick={() =>
-                Taro.chooseAddress({
-                  success({ detailInfo }) {
-                    savaFormData({ address: detailInfo });
-                  },
-                })
-              }
-              extra={<Button className="slp_wx_local">从微信读取</Button>}
-            ></Text>
-          </FormItem>
-        </FormItemGroup>
-        <FormItemGroup>
-          <FormItem label={"自提点联系人"}>
-            <Input
-              value={formData.contactPerson}
-              name="contactPerson"
-              placeholder="请输入自提点联系人"
-            ></Input>
-          </FormItem>
-          <FormItem label={"自提点电话"}>
-            <Input
-              value={formData.mobile}
-              name="mobile"
-              type={"number"}
-              placeholder="请输入自提点电话"
-            ></Input>
-          </FormItem>
-          <FormItem label={"上传图片"} titleTip={"最多3张"} vertical>
-            <Upload
-              count={3}
-              value={formData.images}
-              onChange={(file) => savaFormData({ images: file })}
-            ></Upload>
-          </FormItem>
-        </FormItemGroup>
+        <View className="GoodsManage_box_group">
+          <FormItemGroup>
+            <FormItem label={"名称"} required>
+              <Input
+                value={formData.goodsName}
+                name="goodsName"
+                placeholder="请输入商品名称"
+              ></Input>
+            </FormItem>
+            <FormItem label={"商品描述"} vertical>
+              <Textarea
+                value={
+                  formData.communityGoodsDescObject &&
+                  JSON.parse(formData.communityGoodsDescObject).desc
+                }
+                name={"desc"}
+                placeholder="请输入商品描述"
+                className="bodyClassName"
+                TextareaClassName="txtarea_className"
+                placeholderClass="placeholderClass"
+                disableDefaultPadding
+                maxLength={2000}
+              ></Textarea>
+            </FormItem>
+          </FormItemGroup>
+        </View>
+        <View className="GoodsManage_box_group">
+          <FormItemGroup>
+            <FormItem label={"商品图片"} titleTip={"最多9张"} vertical>
+              <Upload
+                count={9}
+                value={
+                  formData.communityGoodsDescObject &&
+                  JSON.parse(formData.communityGoodsDescObject).img
+                }
+                onChange={(file) => savaFormData({ img: file })}
+              ></Upload>
+            </FormItem>
+          </FormItemGroup>
+        </View>
+        <View className="GoodsManage_box_group">
+          <FormItemGroup>
+            <FormItem label={"价格(¥) "} required>
+              <Input
+                value={formData.costPrice}
+                style={{ color: "#EF476F" }}
+                name="costPrice"
+                placeholder="请输入商品价格 "
+              ></Input>
+            </FormItem>
+            <FormItem label={"库存"}>
+              <Input
+                value={formData.total}
+                name="total"
+                placeholder="不限"
+              ></Input>
+            </FormItem>
+            <FormItem label={"划线价(¥)"}>
+              <Input
+                value={formData.oriPrice}
+                name="oriPrice"
+                placeholder="划线价建议高于商品价格 "
+              ></Input>
+            </FormItem>
+
+            <FormItem label={"成本价(¥)"}>
+              <Input
+                value={formData.price}
+                name="price"
+                placeholder="用于利润核算，仅团长可见"
+              ></Input>
+            </FormItem>
+          </FormItemGroup>
+        </View>
         <FooterFixed>
           <Button formType="submit" className="submit">
             确认{{ edit: "修改", add: "添加" }[type]}
