38aa0b45ccd3aa73be321df6c88337e83b4b81f0
邓新 <dengxin@dakale.net>
2021-09-07 14:59:36 +0800

    no message

--- src/pages/relay/components/shareImage/components/data.js
+++ src/pages/relay/components/shareImage/components/data.js
@@ -20,16 +20,16 @@ export const rssConfigData = (data = {}) => {
     ownerProfile = "https://resource-new.dakale.net/common/default_profile.png";
   }
   return {
-    width: 700, // 画布宽度
-    height: 1177, // 画布高度
-    background: "#FFFFFF",
+    width: 750, // 画布宽度
+    height: 1261, // 画布高度
+    backgroundColor: "#FFFFFF",
     texts: [
       {
-        y: 36 + 20,
+        y: 40 + 20,
         width: 546,
         height: 32,
         zIndex: 20,
-        x: 124,
+        x: 133,
         text: ownerName, //用户名
         fontSize: 32,
         color: "#333333",
@@ -40,11 +40,11 @@ export const rssConfigData = (data = {}) => {
         baseLine: "middle",
       },
       {
-        y: 84 + 20,
+        y: 91 + 20,
         width: 546,
         height: 32,
         zIndex: 20,
-        x: 124,
+        x: 133,
         text: createTime, //创建时间
         fontSize: 24,
         color: "#999999",
@@ -55,11 +55,11 @@ export const rssConfigData = (data = {}) => {
         baseLine: "middle",
       },
       {
-        y: 701 + 20,
+        y: 756 + 20,
         width: 546,
         height: 120,
         zIndex: 20,
-        x: 32,
+        x: 34,
         text: title, //标题
         fontSize: 32,
         color: "#333333",
@@ -70,8 +70,8 @@ export const rssConfigData = (data = {}) => {
         baseLine: "middle",
       },
       {
-        x: 32,
-        y: 833 + 20,
+        x: 34,
+        y: 896 + 20,
         width: 546,
         height: 72,
         zIndex: 110,
@@ -84,8 +84,8 @@ export const rssConfigData = (data = {}) => {
         baseLine: "middle",
       },
       {
-        x: 32,
-        y: 996 + 20,
+        x: 34,
+        y: 1069 + 20,
         text: "¥",
         lineNum: 1,
         fontSize: 24,
@@ -109,11 +109,10 @@ export const rssConfigData = (data = {}) => {
          * 商品图
          */
         url: frontImage, // 商品图
-        width: 700,
-        height: 525,
+        width: 750,
+        height: 563,
         x: 0,
-        y: 144,
-
+        y: 154,
         borderWidth: 0,
         zIndex: 111,
       },
@@ -124,8 +123,8 @@ export const rssConfigData = (data = {}) => {
         url: ownerProfile, // 头像
         width: 80,
         height: 80,
-        y: 32,
-        x: 32,
+        y: 34,
+        x: 34,
         borderRadius: 80,
         borderWidth: 0,
         zIndex: 111,
@@ -135,11 +134,35 @@ export const rssConfigData = (data = {}) => {
          * 二维码
          */
         url: qcodeUrl, // 二维码
-        width: 180,
-        height: 180,
-        y: 957,
-        x: 480,
-        borderRadius: 68,
+        width: 193,
+        height: 193,
+        y: 1026,
+        x: 514,
+        borderWidth: 0,
+        zIndex: 111,
+      },
+      {
+        /**
+         * 头像默认 https://resource-new.dakale.net/common/default_profile.png
+         */
+        url: ownerProfile, // 头像
+        width: 80,
+        height: 80,
+        y: 34,
+        x: 34,
+        borderRadius: 80,
+        borderWidth: 0,
+        zIndex: 111,
+      },
+      {
+        /**
+         * 提示框
+         */
+        url: "https://wechat-config.dakale.net/miniprogram/relay/relay_loct.png", // 提示框
+        width: 304,
+        height: 33,
+        y: 1145,
+        x: 34,
         borderWidth: 0,
         zIndex: 111,
       },
