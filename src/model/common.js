fa47f8cb1f3cc2b2bf253fb7fe6bad50f5a5f9e4
邓新 <dengxin@dakale.net>
2021-11-08 10:50:31 +0800

    删除不需要的文件

--- src/model/common.js
+++ src/model/common.js
@@ -3,6 +3,14 @@ import Taro from "@tarojs/taro";
 const commonStore = observable({
   beanLimit: "500",
   data: {},
+  overallBanner: [],
+  floatList: [],
+  setOverAllBanner(e) {
+    this.overallBanner = e;
+  },
+  setFloatList(e) {
+    this.setFloatList = e;
+  },
   setBean(e) {
     this.beanLimit = e;
   },
