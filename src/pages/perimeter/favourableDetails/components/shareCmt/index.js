3bfdf6cbe994864ed84e279009d8a4bdf4aedbcb
邓新 <dengxin@dakale.net>
2021-11-09 09:36:27 +0800

    非空判断

--- src/pages/perimeter/favourableDetails/components/shareCmt/index.js
+++ src/pages/perimeter/favourableDetails/components/shareCmt/index.js
@@ -33,91 +33,97 @@ export default (props) => {
   if (filterStrList(activityGoodsImg).length > 0 || recommendReason !== "") {
     return (
       <View className="shareCmt_box">
-        <View>
-          <View className="shareCmt_title">图片素材</View>
-          <ScrollView scrollX className="shareCmt_image">
-            <View className="shareCmt_image_view">
-              {filterStrList(activityGoodsImg).map((item, index) => {
-                return (
-                  <View
-                    className="shareCmt_image_content"
-                    onClick={() => {
-                      Taro.previewImage({
-                        urls: filterStrList(activityGoodsImg),
-                        current: index,
-                      });
-                    }}
-                  >
-                    <Image
-                      lazyLoad
-                      mode={"aspectFill"}
-                      src={item}
-                      className="shareCmt_image_info"
-                    ></Image>
-                  </View>
-                );
-              })}
-            </View>
-          </ScrollView>
-          <View
-            className="shareCmt_btn public_center"
-            onClick={() => {
-              getSettingAuth({
-                key: "photo",
-                title: "温馨提示",
-                content: "请授权保存相册权限",
-                success: () => {
-                  savePhoto(filterStrList(activityGoodsImg));
-                },
-                fail: () => {},
-              });
-            }}
-          >
-            保存图片
-          </View>
-        </View>
-        <View>
-          <View className="shareCmt_title shareCmt_margin_info">图片素材</View>
-          <View className="shareCmt_liner_content">
-            {recommendReason}
-            {urlLink && (
-              <View
-                className="shareCmt_cobyLink"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  Taro.setClipboardData({
-                    data: urlLink,
-                    success: function (res) {
-                      toast("复制成功");
-                    },
-                    fail: function (res) {
-                      toast("复制失败");
-                    },
-                  });
-                }}
-              >
-                商品链接
+        {filterStrList(activityGoodsImg).length > 0 && (
+          <View>
+            <View className="shareCmt_title">图片素材</View>
+            <ScrollView scrollX className="shareCmt_image">
+              <View className="shareCmt_image_view">
+                {filterStrList(activityGoodsImg).map((item, index) => {
+                  return (
+                    <View
+                      className="shareCmt_image_content"
+                      onClick={() => {
+                        Taro.previewImage({
+                          urls: filterStrList(activityGoodsImg),
+                          current: index,
+                        });
+                      }}
+                    >
+                      <Image
+                        lazyLoad
+                        mode={"aspectFill"}
+                        src={item}
+                        className="shareCmt_image_info"
+                      ></Image>
+                    </View>
+                  );
+                })}
               </View>
-            )}
+            </ScrollView>
+            <View
+              className="shareCmt_btn public_center"
+              onClick={() => {
+                getSettingAuth({
+                  key: "photo",
+                  title: "温馨提示",
+                  content: "请授权保存相册权限",
+                  success: () => {
+                    savePhoto(filterStrList(activityGoodsImg));
+                  },
+                  fail: () => {},
+                });
+              }}
+            >
+              保存图片
+            </View>
           </View>
-          <View
-            className="shareCmt_btn public_center "
-            onClick={(e) => {
-              e.stopPropagation();
-              Taro.setClipboardData({
-                data: recommendReason + "商品地址" + urlLink && urlLink,
-                success: function (res) {
-                  toast("复制成功");
-                },
-                fail: function (res) {
-                  toast("复制失败");
-                },
-              });
-            }}
-          >
-            复制文案链接
+        )}
+        {recommendReason !== "" && (
+          <View>
+            <View className="shareCmt_title shareCmt_margin_info">
+              图片素材
+            </View>
+            <View className="shareCmt_liner_content">
+              {recommendReason}
+              {urlLink && (
+                <View
+                  className="shareCmt_cobyLink"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    Taro.setClipboardData({
+                      data: urlLink,
+                      success: function (res) {
+                        toast("复制成功");
+                      },
+                      fail: function (res) {
+                        toast("复制失败");
+                      },
+                    });
+                  }}
+                >
+                  商品链接
+                </View>
+              )}
+            </View>
+            <View
+              className="shareCmt_btn public_center "
+              onClick={(e) => {
+                e.stopPropagation();
+                Taro.setClipboardData({
+                  data: recommendReason + "商品地址" + urlLink && urlLink,
+                  success: function (res) {
+                    toast("复制成功");
+                  },
+                  fail: function (res) {
+                    toast("复制失败");
+                  },
+                });
+              }}
+            >
+              复制文案链接
+            </View>
           </View>
-        </View>
+        )}
       </View>
     );
   } else {
