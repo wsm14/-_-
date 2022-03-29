1122d256d2c26982d0d54f39952955f93865fce0
邓新 <dengxin@dakale.net>
2022-03-29 14:44:53 +0800

    改Bug

--- src/pages/index/lookAround/components/selectSpecal.js
+++ src/pages/index/lookAround/components/selectSpecal.js
@@ -2,21 +2,21 @@ import React, { useMemo } from "react";
 import { View } from "@tarojs/components";
 import Waterfall from "@/components/waterfall";
 import Taro from "@tarojs/taro";
-import { selectShop } from "@/components/public_ui/selectSpecal";
+import { template } from "@/components/public_ui/newGoodsObj";
 import Empty from "@/components/Empty";
 import "./../index.scss";
-export default ({ data = [], userInfo = {}, linkTo, type }) => {
+export default ({ data = [], linkTo, type, listObj }) => {
+  const { payBeanCommission, identification } = listObj;
   const memo = useMemo(() => {
-    const template = (item) => {
-      return selectShop(item, userInfo, linkTo);
-    };
     return (
       <View>
         <View className="lookAround_selectSpecal_box lookAround_selectSpecal_fPadding">
           {data.length > 0 ? (
             <Waterfall
               list={data}
-              createDom={template}
+              createDom={(item) =>
+                template(item, { payBeanCommission }, identification)
+              }
               style={{ width: Taro.pxTransform(335) }}
             ></Waterfall>
           ) : (
@@ -29,6 +29,6 @@ export default ({ data = [], userInfo = {}, linkTo, type }) => {
         </View>
       </View>
     );
-  }, [data, userInfo]);
+  }, [data, listObj]);
   return memo;
 };
